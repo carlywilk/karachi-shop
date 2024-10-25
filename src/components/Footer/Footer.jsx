810bbd842f7ca933d9c30ffc35a437035857b009
Carly Wilk <carlymwilk@gmail.com>
2024-10-25 14:56:01 -0400

    Consolidate and add assets, create and style footer for desktop

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,27 @@
 
+import { Link } from "react-router-dom";
+
+import IgLogo from "../../assets/icons/instagram.svg";
+import KarachiShopLogo from "../../assets/logo/karachi-shop-logo.svg";
+import FbLogo from "../../assets/icons/facebook.png";
+
 import "./Footer.scss";
 
 export function Footer() {
     return (
-        <>
-            <p>© Yasir Muhammed. 2024. All Rights Reserved.</p>
-        </>
-    )
+        <section className="footer">
+            <div className="footer__content">
+                <a className="footer__socials" href="https://www.instagram.com/karachishop/" target="_blank">
+                    <img className="footer__icon" src={IgLogo} alt="Instagram logo" />
+                </a>
+                <Link to="/">
+                    <img className="footer__logo" src={KarachiShopLogo} alt="Karachi Shop Logo" />
+                </Link>
+                <a className="footer__socials" href="https://www.facebook.com/karachishop.yasir/" target="_blank">
+                    <img className="footer__icon" src={FbLogo} alt="Facebook logo" />
+                </a>
+            </div>
+            <p className="footer__copyright">© Yasir Muhammed. 2024. All Rights Reserved.</p>
+        </section>
+    );
 }
