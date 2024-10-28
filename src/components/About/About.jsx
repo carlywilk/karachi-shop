9d1c2fbdce21eabdd55ccbd938ce827fda4d66a3
Carly Wilk <carlymwilk@gmail.com>
2024-10-28 00:34:33 -0400

    Create and style about section at all breakpoints

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,15 @@
 
+import YasirAbout from "../../assets/images/About-BW-slate.png";
+
 import "./About.scss";
 
 export function About() {
     return (
         <section className="about" id="about-section">
-            
+            <img className="about__overlay-image" src={YasirAbout} alt="" />
+            <p className="about__overlay-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia optio, veniam quidem eius sequi voluptates eligendi tenetur quis! Suscipit voluptas facilis labore rem magni! Excepturi est eaque nostrum dolore laboriosam!
+            </p>
         </section>
     )
 }
