3ae26ab488b773ac42bf0fbbc5667c1209b642e6
Carly Wilk <carlymwilk@gmail.com>
2024-11-05 23:08:09 -0500

    Minor styling changes to header and about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,9 +7,14 @@ export function About() {
     return (
         <section className="about" id="about-section">
             <img className="about__overlay-image" src={YasirAbout} alt="" />
-            <p className="about__overlay-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia optio, veniam quidem eius sequi voluptates eligendi tenetur quis! Suscipit voluptas facilis labore rem magni! Excepturi est eaque nostrum dolore laboriosam!
-            </p>
+            <div className="about__overlay-text">
+                <h1 className="about__subtitle">Vintage motorcycle repair</h1>
+                <h1 className="about__subtitle">&</h1>
+                <h1 className="about__subtitle">custom fabrication</h1>
+                <p className="about__body-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia optio, veniam quidem eius sequi voluptates eligendi tenetur quis! Suscipit voluptas facilis labore rem magni! Excepturi est eaque nostrum dolore laboriosam!
+                </p>
+            </div>
         </section>
     )
 }
