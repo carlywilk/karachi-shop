f49205f6e92b2b35af1b8dca338dfa5d6dbfe802
Carly Wilk <carlymwilk@gmail.com>
2024-12-05 03:39:05 -0500

    fix unused error in app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { HomePage } from "./pages/HomePage/HomePage.jsx";
+// import { HomePage } from "./pages/HomePage/HomePage.jsx";
 import { PageNotFound } from "./pages/PageNotFound/PageNotFound.jsx";
 import { Construction } from "./pages/Construction/Construction.jsx";
-
-// import { Footer } from './components/Footer/Footer.jsx';
 
 import './App.scss';
 
@@ -16,7 +14,6 @@ function App() {
         <Route path="/" element={<Construction />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
