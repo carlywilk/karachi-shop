ebdec708a649edef5fd21e431a916519cf5e10db
Carly Wilk <carlymwilk@gmail.com>
2024-12-06 02:18:36 -0500

    Return site to starting point, debugging incomplete

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,32 +2,7 @@
 
 import "./Contact.scss";
 
-export function Contact() {
-    return (
-        <form name="contact" method="POST" data-netlify="true">
-            <p>
-                <label>Your Name: <input type="text" name="name" /></label>
-            </p>
-            <p>
-                <label>Your Email: <input type="email" name="email" /></label>
-            </p>
-            <p>
-                <label>Your Role: <select name="role[]" multiple>
-                <option value="leader">Leader</option>
-                <option value="follower">Follower</option>
-                </select></label>
-            </p>
-            <p>
-                <label>Message: <textarea name="message"></textarea></label>
-            </p>
-            <p>
-                <button type="submit">Send</button>
-            </p>
-            </form>
-    )
-}
 
-/*
 export function Contact() {
     return (
         <section className="contact">
@@ -58,7 +33,7 @@ export function Contact() {
         </section>
     );
 }
-*/
+
 
 /*
 export function Contact() {
