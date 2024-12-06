c9eb6fe4237cc54e74a6693351599445cc3d87fe
Carly Wilk <carlymwilk@gmail.com>
2024-12-06 02:04:15 -0500

    Fix form submission endpoint

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,7 +5,7 @@ import "./Contact.scss";
 export function Contact() {
     return (
         <section className="contact">
-            <form className="contact__form" name="contact" method="POST" netlify>
+            <form className="contact__form" name="contact" method="POST" netlify action="/">
             <input
                     className="contact__name"
                     type="text"
