0e031d92597939929e89d21b4a91c31dc1591683
Carly Wilk <carlymwilk@gmail.com>
2024-12-10 01:35:29 -0500

    Debug form - simplify code

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,21 +1,10 @@
-import { useRef } from "react";
 
 import "./Contact.scss";
 
 export function Contact() {
-    const formRef = useRef(null);
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        if (formRef.current) {
-            formRef.current.reset();
-        }
-    };
-
     return (
         <section className="contact" id="contact-section">
-            <form className="contact__form" ref={formRef} name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
+            <form className="contact__form" name="contact" method="POST" data-netlify="true">
             <input type="hidden" name="form-name" value="contact" />
             <input
                     className="contact__name"
