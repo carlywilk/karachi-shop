c6c94f9b662772c8e7ab20af18b01930508c8b1a
Carly Wilk <carlymwilk@gmail.com>
2024-12-10 22:57:44 -0500

    Style mobile and tablet breakpoints for For Sale section

--- src/components/ForSale/ForSale.jsx
+++ src/components/ForSale/ForSale.jsx
@@ -1,10 +1,19 @@
+import exampleOne from "../../assets/stockimages/daniel-TKsBL1HXo5Y-unsplash.jpg";
+import exampleTwo from "../../assets/stockimages/jane-duursma-eD5eD7OVdMQ-unsplash.jpg";
 
 import "./ForSale.scss";
 
 export function ForSale() {
     return (
         <section className="sale" id="sale-section">
-            <p>this is the for sale section</p>
+            <div className="sale__bike">
+                <img className="sale__image" src={exampleOne} alt="" />
+                <p className="sale__description sale__description--right-aligned">bike description; this is a sample image used to design the site prior to client providing assets</p>
+            </div>
+            <div className="sale__bike sale__bike--flipped">
+                <img className="sale__image" src={exampleTwo} alt="" />
+                <p className="sale__description sale__description--left-aligned">bike description; this is a sample image used to design the site prior to client providing assets</p>
+            </div>
         </section>
-    )
+    );
 }
