db02bfcdf89c8663012551463cd2d0b0ac071108
Carly Wilk <carlymwilk@gmail.com>
2024-12-12 21:03:37 -0500

    Style projects section at all breakpoints

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,9 @@
 
-import fillerOne from "../../assets/stockimages/austin-neill-_uHFBjtwZ1E-unsplash.jpg";
-import fillerTwo from "../../assets/stockimages/christopher-burns-Pl0s3qM0A_w-unsplash.jpg";
-import fillerThree from "../../assets/stockimages/christopher-burns-ZrfnOyYUfi4-unsplash.jpg";
-import fillerFour from "../../assets/stockimages/heqi-valiansyah-A7zzwQm9k5I-unsplash.jpg";
+import whiteKnightOne from "../../assets/images/white-knight-1.jpg";
+// import whiteKnightTwo from "../../assets/images/white-knight-2.jpg";
+import firstCustom from "../../assets/images/first-custom.jpg";
+import bigKitty from "../../assets/images/big-kitty.jpg";
+import oldYeller from "../../assets/images/old-yeller.jpg";
 
 import "./Projects.scss";
 
@@ -12,12 +13,12 @@ export function Projects() {
             <h1 className="projects__title">My Work</h1>
             <div className="projects__examples">
                 <div className="projects__upper">
-                    <img className="projects__image" src={fillerOne} alt="testImage" />
-                    <img className="projects__image projects__image--spaced" src={fillerTwo} alt="testImage" />
+                    <img className="projects__image" src={whiteKnightOne} alt="testImage" />
+                    <img className="projects__image projects__image--spaced" src={firstCustom} alt="testImage" />
                 </div>
                 <div className="projects__lower">
-                    <img className="projects__image" src={fillerThree} alt="testImage" />
-                    <img className="projects__image projects__image--spaced" src={fillerFour} alt="testImage" />
+                    <img className="projects__image" src={bigKitty} alt="testImage" />
+                    <img className="projects__image projects__image--spaced" src={oldYeller} alt="testImage" />
                 </div>
             </div>
         </section>
