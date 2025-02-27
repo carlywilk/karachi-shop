4f8380dc0fa5b821e16d45dc92531617b1598e7e
Carly Wilk <carlymwilk@gmail.com>
2025-02-27 11:37:46 -0500

    Update For Sale section to client asset

--- src/components/ForSale/ForSale.jsx
+++ src/components/ForSale/ForSale.jsx
@@ -1,5 +1,4 @@
-import exampleOne from "../../assets/stockimages/daniel-TKsBL1HXo5Y-unsplash.jpg";
-import exampleTwo from "../../assets/stockimages/jane-duursma-eD5eD7OVdMQ-unsplash.jpg";
+import suzuki1987 from "../../assets/images/for-sale-suzuki.jpg";
 
 import "./ForSale.scss";
 
@@ -8,13 +7,19 @@ export function ForSale() {
         <section className="sale" id="sale-section">
             <h1 className="sale__title">For Sale</h1>
             <div className="sale__bike">
-                <img className="sale__image" src={exampleOne} alt="" />
-                <p className="sale__description sale__description--right-aligned">bike description; this is a sample image used to design the site prior to client providing assets</p>
+                <img className="sale__image" src={suzuki1987} alt="" />
+                <div className="sale__description sale__description--right-aligned">
+                    <p className="sale__description-title">1987 Suzuki DR600 Dakar</p>
+                    <p className="sale__description-text">I purchased this bike a few years ago as a project, and it's been fully restored. It's been reassembled with attention to detail, including new seals, tires, chain, and sprockets. The bike is in great condition and is being sold certified, with the UVIP included. Ready to ride.</p>
+                </div>
             </div>
-            <div className="sale__bike sale__bike--flipped">
-                <img className="sale__image" src={exampleTwo} alt="" />
-                <p className="sale__description sale__description--left-aligned">bike description; this is a sample image used to design the site prior to client providing assets</p>
-            </div>
+            {/* <div className="sale__bike sale__bike--flipped">
+                <img className="sale__image" src={suzuki1987} alt="" />
+                <div className="sale__description sale__description--left-aligned">
+                    <p className="sale__description-text">1987 Suzuki DR600 Dakar</p>
+                    <p className="sale__description-text">I purchased this bike a few years ago as a project, and it's been fully restored. It's been reassembled with attention to detail, including new seals, tires, chain, and sprockets. The bike is in great condition and is being sold certified, with the UVIP included. Ready to ride.</p>
+                </div>
+            </div> */}
         </section>
     );
 }
